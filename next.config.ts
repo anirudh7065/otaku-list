1810e065c0a7acc34b3f24ae2695ea243e23e779
Abhishek Valsan <107138932+anirudh7065@users.noreply.github.com>
2026-03-02 11:50:44 +0530

    Update next.config.ts added a remote pattern

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,10 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "cdn.myanimelist.net",
         pathname: "/**",
+      },{
+        protocol: "https",
+        hostname: "myanimelist.net",
+        pathname: "/**",
       },
     ],
   },
